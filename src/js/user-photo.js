e25e6f5bcc58e769c3dd8a8536cbc0c78fd2d333
Dan Rumney <dancrumb@gmail.com>
2013-02-16 15:13:32 -0600

    Issue #3: Adding pre-upload preview - Support arbitrary sizes of input images to the cropper widget

--- src/js/user-photo.js
+++ src/js/user-photo.js
@@ -1,21 +1,66 @@
 /**jshint jQuery:true */
 (function($) {
+    /**
+     * @type {Jcrop}
+     */
     var cropper;
+
+    /**
+     * Restrict the boundaries of an image to those defined by `limits`
+     *
+     * @param {Image} image
+     * @param {Object} limits
+     * @param {number} limits.height
+     * @param {number} limits.width
+     * @return {Object}
+     */
+    var imageLimiter = function(image, limits) {
+        var imgDims = {
+            width:  image.width,
+            height: image.height
+        };
+        var targetDim = (imgDims.width > imgDims.height) ? 'width' : 'height';
+        var scaleFactor = Math.max(1,imgDims[targetDim] / limits[targetDim]);
+
+        return {
+            width: imgDims.width / scaleFactor,
+            height: imgDims.height / scaleFactor
+        };
+    };
 
     var fileReader = new FileReader();
     fileReader.onload = function(frEvent) {
-        console.log(frEvent);
-        cropper.destroy();
-        $('#user-photo-preview').attr('src', frEvent.target.result);
-        cropper = $('#user-photo-preview').Jcrop(
-            {
-                aspectRatio: 1,
-                boxWidth: 250,
-                boxHeight: 250
-            }, function() {
-                cropper = this;
-            }
-        );
+        if(cropper) {
+            cropper.destroy();
+        }
+        var previewImage = $('#user-photo-preview');
+        var sizingImage = new Image();
+        $(sizingImage).on('load', function() {
+
+            /*
+             * Now that the sizingImage has loaded, we can figure out the actual size of the image
+             * and make sure it renders properly.
+             */
+            var previewSize = imageLimiter(sizingImage, {width:300, height: 300});
+
+            previewImage.width(previewSize.width  + 'px')
+                        .height(previewSize.height + 'px')
+                        .show();
+            cropper = $.Jcrop('#user-photo-preview',
+                {
+                    aspectRatio: 1,
+                    trueSize: [sizingImage.width, sizingImage.height],
+                    onSelect: function() {
+                        console.log(arguments);
+                    }
+                }, function() {
+                    cropper = this;
+                }
+            );
+        });
+        previewImage.attr('src', frEvent.target.result);
+        sizingImage.src =  frEvent.target.result;
+
     };
 
     var loadImage = function(fileInputId) {
@@ -27,11 +72,10 @@
 
     $(function() {
         console.log("User Photo running...");
-        cropper = $('#user-photo-preview').Jcrop(
-            {aspectRatio: 1}, function() {
-                cropper = this;
-            }
-        );
+        $('#user-photo-preview')
+            .width('300px')
+            .height('300px')
+            .hide();
         $('#userphoto_image_file').on('change', function() {
             loadImage('userphoto_image_file');
         });
